e0682374161a9fd1d74e910e2261466f184562e1
Sybia Roberson <sybia.roberson@gmail.com>
2015-08-08 12:33:29 -0500

    sybia's css, html, js edits

--- tictactoe.js
+++ tictactoe.js
@@ -1,4 +1,22 @@
-var player1Name = prompt("Player 1 Enter Your Name");
-var player2Name = prompt("Player 2 Enter Your Name");
+// var player1Name = prompt("Player 1 Enter Your Name");
+// var player2Name = prompt("Player 2 Enter Your Name");
 
 
+// var clickBox = 
+// 	document.getElementsByClassName("box");
+
+// var tdClickHandler = function(e) {
+//   outputEl.innerHTML = "You clicked X";
+// };	
+
+
+var allBox = document.getElementsByClassName("box");
+console.log(allBox);
+
+function boxClick(e) {
+  alert("hello");
+}
+
+for (var i = 0; i < allBox.length; i++) {
+  allBox.item(i).addEventListener("click", boxClick);
+}
