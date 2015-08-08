40c76d5bf7fc7417b34b1df431860eb07c7d36d0
Brian Merchant <btmerchant@comcast.net>
2015-08-08 12:42:39 -0500

    Brian work on all files this will conflict

--- tictactoe.js
+++ tictactoe.js
@@ -1,4 +1,25 @@
-var player1Name = prompt("Player 1 Enter Your Name");
-var player2Name = prompt("Player 2 Enter Your Name");
+
+var grid =[];
+
+for (i = 0; i < 9; i ++) {
+    grid[i] = "/0";
+    console.log(grid[i]);
+}
+
+var playerUp = 1;
+var playerSimbol = "X";
+
+//var player1Name = prompt("Player 1 Enter Your Name");
+//var player2Name = prompt("Player 2 Enter Your Name");
+
+playerUp = Math.floor((Math.random() * 2) + 1);
+console.log(playerUp);
+
+document.getElementById("prompt").innerHTML += "Player " + playerUp + " its your turn" + "<br>";
+
+document.getElementById("prompt").innerHTML += "Click on a Square";
+
+document.getElementById("b1").innerHTML = "X";
+
 
 
